09f7b4c6939b61d2d9d750fb83c138bbcabddeef
Marko Sentivanac <markosentivanac1@gmail.com>
2024-02-16 20:09:29 +0100

    adds package dependencies

---
+++ NBAApp/NBAAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBAAppApp.swift
+//  NBAApp
+//
+//  Created by Marko Sentivanac on 16.2.24..
+//
+
+import SwiftUI
+
+@main
+struct NBAAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
